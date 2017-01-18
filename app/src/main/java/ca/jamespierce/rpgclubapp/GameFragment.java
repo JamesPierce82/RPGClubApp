8750ae87843dd1ca900b0e6f18d9ee3a8e760fa0
James Pierce <James.pierce82@stclairconnect.ca>
2017-01-17 23:18:07 -0500

    GameFragment Code Update
    * Added the remaining code required for GameFragment to function.
    * Will test to confirm app behaves as expected.
    * Will require proper content to be designed and put in place.

--- app/src/main/java/ca/jamespierce/rpgclubapp/GameFragment.java
+++ app/src/main/java/ca/jamespierce/rpgclubapp/GameFragment.java
@@ -8,6 +8,8 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 /**
@@ -23,10 +25,12 @@ public class GameFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_PARAM3 = "param2";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private int mParam3;
 
 
     private OnFragmentInteractionListener mListener;
@@ -44,11 +48,12 @@ public class GameFragment extends Fragment {
      * @return A new instance of fragment GameFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static GameFragment newInstance(String param1, String param2) {
+    public static GameFragment newInstance(String param1, String param2, int param3) {
         GameFragment fragment = new GameFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
+        args.putInt(ARG_PARAM3, param3);
         fragment.setArguments(args);
         return fragment;
     }
@@ -59,6 +64,7 @@ public class GameFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+            mParam3 = getArguments().getInt(ARG_PARAM3);
         }
     }
 
@@ -66,7 +72,25 @@ public class GameFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false);
+       View view = inflater.inflate(R.layout.fragment_game, container, false);
+
+        if(mParam1 != null) {
+            TextView text = (TextView) view.findViewById(R.id.title);
+            text.setText(mParam1);
+        }
+        if(mParam2 != null) {
+            TextView text = (TextView) view.findViewById(R.id.description);
+            text.setText(mParam2);
+        }
+        // Not sure how to best deal with this, 0 seems to work but leaves
+        // the rare case where it could fail.
+        if(mParam3 != 0) {
+            ImageView image = (ImageView) view.findViewById(R.id.gameImage);
+            image.setImageResource(mParam3);
+        }
+
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
