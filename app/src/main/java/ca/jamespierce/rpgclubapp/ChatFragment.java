54ff8d61a292e2f1f8e5d1ec7343624be7eb74a7
James Pierce <james.pierce82@stclairconnect.ca>
2017-01-23 20:42:23 -0500

    MessageList progress
    * Message entered into the editText will be addd to the messageList array.
    * Still needs to clear the editText and refresh the adapter

--- app/src/main/java/ca/jamespierce/rpgclubapp/ChatFragment.java
+++ app/src/main/java/ca/jamespierce/rpgclubapp/ChatFragment.java
@@ -11,11 +11,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 
@@ -37,8 +41,18 @@ public class ChatFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    // Declare variables for views being displayed
     RecyclerView rvMessages;
+    Button sendButton;
 
+    // Declare variables for the message content to be transferred
+    EditText messageContent;
+    String newMessage;
+
+    // Declare variables to store the user's information
+    // This will later be used to connect to the settings
+    String userName = "James";
+    int userAvatar = R.drawable.bob;
 
     private OnFragmentInteractionListener mListener;
 
@@ -91,6 +105,7 @@ public class ChatFragment extends Fragment {
         FloatingActionButton fab = (FloatingActionButton) getActivity().findViewById(R.id.fab);
         fab.hide();
 
+        sendButton = (Button) view.findViewById(R.id.sendButton);
         rvMessages = (RecyclerView) view.findViewById(R.id.chatList);
 
         // Create an ArrayList that holds messages
@@ -121,8 +136,23 @@ public class ChatFragment extends Fragment {
         // Set layout manager to position the items
         rvMessages.setLayoutManager(new LinearLayoutManager(this.getContext()));
 
-        // Unlike the other apps we have done, This one does not require a onclick listener currently.
-        // It does not need to perform any actions when touching the message.
+        // This will be used to send a message once the user is ready to send it.
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newMessage =  messageContent.toString();
+
+                // This will get the current time.
+                String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+
+                // This will add the message to the messageList.
+                messageList.add(new Message(userName, currentDateTimeString, newMessage, userAvatar));
+
+                // This will update the adapter so that the new message will be displayed on the screen
+                
+            }
+        });
+
 
         return view;
     }
