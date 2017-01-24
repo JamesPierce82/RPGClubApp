623a342c45127b2740ba186711878f575314f198
James Pierce <james.pierce82@stclairconnect.ca>
2017-01-24 15:32:26 -0500

    reverted SplashActivity to normal, commented out code to add later

--- app/src/main/java/ca/jamespierce/rpgclubapp/SplashActivity.java
+++ app/src/main/java/ca/jamespierce/rpgclubapp/SplashActivity.java
@@ -9,9 +9,10 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+//        finish();
     }
 }
