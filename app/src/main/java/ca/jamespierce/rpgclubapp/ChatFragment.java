8d7d304fcbc2181506a0d0a64015c8142e5adfbf
James Pierce <james.pierce82@stclairconnect.ca>
2017-01-23 19:39:47 -0500

    transfer commit to content

--- app/src/main/java/ca/jamespierce/rpgclubapp/ChatFragment.java
+++ app/src/main/java/ca/jamespierce/rpgclubapp/ChatFragment.java
@@ -127,6 +127,8 @@ public class ChatFragment extends Fragment {
         return view;
     }
 
+
+
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
