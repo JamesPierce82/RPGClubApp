fd462f46e404523223ec87b93efc26179576622b
--global <--global>
2017-02-09 15:06:09 -0500

    Added Adapter to EquipmentFragment
    * This will allow the equipment to be propely displayed in the app

--- app/src/main/java/ca/jamespierce/rpgclubapp/EquipmentFragment.java
+++ app/src/main/java/ca/jamespierce/rpgclubapp/EquipmentFragment.java
@@ -1,13 +1,20 @@
 package ca.jamespierce.rpgclubapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -29,7 +36,9 @@ public class EquipmentFragment extends Fragment {
     private String mParam2;
 
     ListView list;
-    TextView
+
+    // Declare variables for items that will appear in the CardView. These will be used in the Adapter
+    TextView equipmentDescriptionTextview;
 
     private OnFragmentInteractionListener mListener;
 
@@ -64,15 +73,69 @@ public class EquipmentFragment extends Fragment {
         }
     }
 
+    FragmentManager fm;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_equipment, container, false);
 
+        equipmentDescriptionTextview = (TextView) view.findViewById(R.id.descriptionText);
 
+        final ArrayList<Equipment> equipmentList = new ArrayList<Equipment>();
+
+        // Add items to the arraylist
+        equipmentList.add(new Equipment("Pen or Pencil", "This is a writing utensil that you will need in order to write down and help keeptrack of your stats in your game of choice. You should bring a few spares in case you run out of ink or lead."));
+        equipmentList.add(new Equipment("Character Sheets", "These are usually game specific. Bring your character sheet(If you have already made one), or bring an empty one if you still need to create a character."));
+        equipmentList.add(new Equipment("Dice", "You should bring your dice sets. These range from sets of D6 to a full range of D2 - D20 or even D100(typically we use two D10's instead as a D100 is more like a golf ball and can take a while to stop."));
+        equipmentList.add(new Equipment("Character Model", "If you are playing a game that uses character models, make sure you bring one for your character. This is so that you can customize your character as you want."));
+        equipmentList.add(new Equipment("test1", "hello1"));
+        equipmentList.add(new Equipment("test2", "hello2"));
+        equipmentList.add(new Equipment("test3", "hello3"));
+        equipmentList.add(new Equipment("test4", "hello4"));
+
+        // This assigns an adapter to the ArrayList above
+        final EquipmentAdapter adapter = new EquipmentAdapter(getContext(), equipmentList);
+        // This links the adapter to the List in the xml
+        list.setAdapter(adapter);
 
         return view;
+    }
+
+    //EquipmentAdapter for the listview
+    public class EquipmentAdapter extends ArrayAdapter<Equipment> {
+
+        public EquipmentAdapter(Context context, ArrayList<Equipment> items) {
+            super(context, 0, items);
+        }
+        public View getView(int position, View convertView, ViewGroup parent){
+            final Equipment item = getItem(position);
+
+            if(convertView == null){
+                convertView =
+                        LayoutInflater.from(getContext()).inflate(
+                                R.layout.item_view, parent, false);
+            }
+            // This is where we set the name textview value
+            TextView itemName = (TextView) convertView.findViewById(R.id.equipmentName);
+            itemName.setText(item.getName());
+            // This will be the image that opens a new page when tapped(Globe icon)
+//            ImageView image = (ImageView) convertView.findViewById(R.id.location);
+//            image.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    Uri geoLocation = Uri.parse(item.getLocation());
+//                    Intent intent = new Intent(Intent.ACTION_VIEW);
+//                    intent.setData(geoLocation);
+//                    if(intent.resolveActivity(getActivity().getPackageManager()) != null) {
+//                        startActivity(intent);
+//                    }
+//                }
+//            });
+
+            return  convertView;
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
