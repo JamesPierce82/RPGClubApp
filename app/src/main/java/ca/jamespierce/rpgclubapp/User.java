96ddfe998a214d242d5e16503ab3247b086828af
web <web@amso0055z-16.local>
2017-02-16 10:55:46 -0500

    Added User Class

---
+++ app/src/main/java/ca/jamespierce/rpgclubapp/User.java
@@ -0,0 +1,19 @@
+package ca.jamespierce.rpgclubapp;
+
+/**
+ * Created by web on 2017-02-16.
+ */
+
+public class User {
+
+    // Declare variables
+    private String name;
+    private int avatar;
+
+    // Constructor
+    public User(String name, int avatar) {
+        this.name = name;
+        this.avatar = avatar;
+    }
+
+}
