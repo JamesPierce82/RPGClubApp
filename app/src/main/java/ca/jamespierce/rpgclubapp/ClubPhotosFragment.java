6bc3e2a9b8808916ba517c8037f361755db3d7f3
--global <--global>
2017-03-22 23:30:58 -0400

    BROKEN Added Picture taking code
    * This code will allow the app to take a picture and would attempt to store it in a database if it could.

--- app/src/main/java/ca/jamespierce/rpgclubapp/ClubPhotosFragment.java
+++ app/src/main/java/ca/jamespierce/rpgclubapp/ClubPhotosFragment.java
@@ -1,13 +1,26 @@
 package ca.jamespierce.rpgclubapp;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.app.Activity.RESULT_OK;
 import static ca.jamespierce.rpgclubapp.MainActivity.fab;
 
 
@@ -28,6 +41,9 @@ public class ClubPhotosFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private static final int CAMERA_INTENT = 1;
+    private String imageLocation;
 
     private OnFragmentInteractionListener mListener;
 
@@ -70,8 +86,95 @@ public class ClubPhotosFragment extends Fragment {
 
         fab.show();
 
+        fab.setImageResource(R.drawable.ic_add_a_photo_black_24dp);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File picture = null;
+                try{
+                    picture = createImage();
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+                Intent i = new Intent();
+                i.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                i.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(picture));
+                if(i.resolveActivity(getActivity().getPackageManager()) != null){
+                    startActivityForResult(i, CAMERA_INTENT);
+                }
+
+
+
+            }
+        });
+
         return view;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == CAMERA_INTENT && resultCode == RESULT_OK){
+            Bitmap image = BitmapFactory.decodeFile(imageLocation);
+            /*
+             * This is the part of the code where it originally added the image to the screen.
+             * Because I am adding mine to a Database, then to a list view which can be refreshed,
+             * I should not need to do this here.
+             */
+            /**
+             * Add the photo to the database
+             */
+            DatabaseHandler db = new DatabaseHandler(getContext());
+            int picID = db.addPicture(new Picture(imageLocation));
+            if(picID != -1){
+                // This should be removed. Should not require this as we are only adding the image to one table.
+                // Can use this to produce a toast message though.
+//                Location location = (Location) spin.getSelectedItem();
+//                db.addImageLocation(picID, location.getId());
+                Toast.makeText(getActivity(), "Photo Added",
+                        Toast.LENGTH_LONG).show();
+            }
+            else{
+                Toast.makeText(getActivity(), "Photo Not Added",
+                        Toast.LENGTH_LONG).show();
+            }
+
+            /*
+             * Refer to the Chat window to see how to refresh list content.
+             * This needs to be done after the image has been added to the database.
+             */
+            db.closeDB();
+        }
+    }
+
+    /**
+     * This method is used to create a temp
+     * file that the Camera will use to save a photo
+     * We will generate a file with a collision free name
+     * (rpg_club_app_photo_timestamp.jpg)
+     * This method throws an IOException because we might not
+     * be able to create the file
+     * @return
+     * @throws IOException
+     */
+    File createImage() throws IOException{
+        //Create a timestamp to help create a collision free name
+        String timeStamp =
+                new SimpleDateFormat("yyyyMMdd_HHss").format(new Date());
+        //Create the name of the image
+        String fileName = "rpg_club_app_photo_" + timeStamp;
+        //Grab the directory we want to save the image
+        File directory =
+                Environment.
+                        getExternalStoragePublicDirectory
+                                (Environment.DIRECTORY_PICTURES);
+        //Create the image in that directory
+        File picture = File.createTempFile(fileName, ".jpg", directory);
+        //Save the location of the image
+        imageLocation = picture.getAbsolutePath();
+        return picture;
+    }
+
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
